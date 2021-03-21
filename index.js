c26e8584b5dceda8b9d3d57aaba79bff3a265ee4
Neil <zooming.in@hotmail.com>
2021-03-21 20:17:27 +1100

    Initialise git

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+const employee = {
+    name: "Sam",
+    address: "11 Broadway"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newObj = {...employee}
+    newObj[key] = value;
+    return newObj;
+}
+
+function destrictivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee.address = "12 Broadway";
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key, value) {
+    delete employee.address;
+    return employee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+
+}
